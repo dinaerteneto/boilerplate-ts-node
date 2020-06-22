49e2068c1bd22a8464d8fd17e76044dd9c3efb96
Dinaerte Neto <dinaerteneto@gmail.com>
2020-06-22 05:26:10 -0300

    adicionando typeorm

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 
+import './database';
+
 const app = express();
 
 app.get('/', (request, response) => {
   return response.json({ message: 'Hello World!' });
 });
 
-app.listen(3333, () => {
-  console.log('Sever started on port 3333');
+app.listen(3002, () => {
+  console.log('Sever started on port 3002');
 });

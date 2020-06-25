73586f856c2bc63577003cb008c9c71dba770e11
Dinaerte Neto <dinaerteneto@gmail.com>
2020-06-25 16:57:58 -0300

    add erros do celebrate no .use do express

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import express from 'express';
+import { errors } from 'celebrate';
 import routes from './routes';
 
 import '@shared/infra/typeorm';
@@ -9,6 +10,7 @@ const app = express();
 app.use(express.json());
 
 app.use(routes);
+app.use(errors());
 
 app.listen(3002, () => {
   console.log('Sever started on port 3002');
